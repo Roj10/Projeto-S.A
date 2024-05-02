5175be3503ae89a7e21a4cf8cc072d8f06500b15
Renan Jussiani <150622957+Roj10@users.noreply.github.com>
2024-05-02 20:58:14 -0300

    ajuste

--- script.js
+++ script.js
@@ -9,29 +9,29 @@ const updatecalendar = () => {
     const currentyear = currentdate.getFullYear();
     const currentmonth = currentdate.getMonth();
 
-    const firstday = new Date(currentyear, currentmonth,0);
-    const lastday = new Date(currentyear, currentmonth + 1,0);
+    const firstday = new Date(currentyear, currentmonth, 0);
+    const lastday = new Date(currentyear, currentmonth + 1, 0);
     const totaldays = lastday.getDate();
     const firstdayindex = firstday.getDay();
     const lastdayindex = lastday.getDay();
 
-    const monthyearstring = currentdate.tolocalestring('default', {month: 'long', year: 'numeric'});
+    const monthyearstring = currentdate.tolocalestring('default', { month: 'long', year: 'numeric' });
     monthyearelement.textContent = monthyearstring;
 
     let datesHTML = '';
 
-    for(let i = firstdayindex; 1 > 0; i--) {
+    for (let i = firstdayindex; 1 > 0; i--) {
         const prevdate = new Date(currentyear, currentmonth, 0 - i + 1);
         datesHTML += `<div class="date inactive">${prevdate.getDate()}</div>`;
     }
 
-    for(let i = 1; i <= totaldays; i++) {
+    for (let i = 1; i <= totaldays; i++) {
         const date = new Date(currentyear, currentmonth, i);
         const activeclass = date.toDateString() === new Date().toDateString ? 'active' : '';
         datesHTML += `<div class="date ${activeclass}">${i}</div>`;
     }
 
-    for(let i = 1; i <= 7 - lastdayindex; i++) {
+    for (let i = 1; i <= 7 - lastdayindex; i++) {
         const nextdate = new Date(currentyear, currentmonth + 1, i);
         datesHTML += `<div class="date inactive">${nextdate.getDate()}</div>`;
     }
@@ -42,11 +42,11 @@ const updatecalendar = () => {
 prevbtn.addEventListener('click', () => {
     currentdate.getDate.setmonth(currentdate.getMonth() - 1);
     updatecalendar();
-}) 
+})
 
 nextbtn.addEventListener('click', () => {
     currentdate.getDate.setmonth(currentdate.getMonth() + 1);
     updatecalendar();
-}) 
+})
 
 updatecalendar();
