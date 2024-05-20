0c0aaa4966a7b0973654664c3a42ce54d20a2a29
Renan Jussiani <150622957+Roj10@users.noreply.github.com>
2024-05-20 19:17:05 -0300

    ajuste

--- ja/src/pages/home.jsx
+++ ja/src/pages/home.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 function Home() {
-  const irPara= useNavigate();
+  const irPara = useNavigate();
   const handleClick = () => {
     irPara("/");
   };
   return (
     <div>
-      <h1>Home</h1>      
-      <button onClick= {handleClick}>Fazer login</button>
+      <h1>Home</h1>
+      <button onClick={handleClick}>Fazer login</button>
     </div>
   );
 }
